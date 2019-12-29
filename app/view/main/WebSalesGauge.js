37ab37fe23773b7038c80ae8ad1cc9e3ee666845
Avi Michael <avi@QQQ.local>
2019-12-29 19:01:24 +1100

    restful component GETs AREASAPI customer data (one entry), also has JSON RSS reader commented out.

--- app/view/main/WebSalesGauge.js
+++ app/view/main/WebSalesGauge.js
@@ -1,29 +1,39 @@
+// THIS IS A GAUGE COMPONENT NOT A GAUGE CHART
+// THIS IS EXT.UX.GAUGE.GAUGE
+
 Ext.define('MyApp.view.WebSalesGauge', {
     extend: 'Ext.Panel',
     alias: 'widget.needle-gauge',
     controller: 'anchored-tooltips',    
-
-
 
     requires: [
         'Ext.ux.gauge.Gauge',
         'Ext.ux.gauge.needle.Spike'
     ],
 
+    // title: 'test2',
+    ui: 'light',
     width: '100%',
-    height: 150,
-    layout: {
-        type: 'hbox'
-    },
+    height: 190,
+    
+    // layout: {
+    //     type: 'hbox',
+    //     align: 'stretch'
+    // },
 
-    captions: {
-        title: 'Profits and Losses (per product category)'
-    },
+    // bbar: [
+    //     {
+    //     xtype: 'textfield',
+    //     name: 'Total',
+    //     fieldLabel: 'Total',
+    //     allowBlank: false 
+    //     }
+    // ],
 
     viewModel: {
         data: {
             value: 30
-        }
+        },
     },
 
     // html: [{
@@ -32,13 +42,87 @@ Ext.define('MyApp.view.WebSalesGauge', {
 
 
     items: [{
- 
-        reference: 'webSales',
+        reference: 'webSalesGauge',
         xtype: 'gauge',
         flex: 1,
+        
+        colors: ['#1F6D91', '#90BCC9'],
         bind: '{value}',
-        needle: 'spike',
-        cls: "Sales"
-    }]
+        
+        // radiusFactor: 1,
+        
+        textOffset: {
+            dy: 45
+        },
+        needle: {
+            type: 'spike',
+            outerRadius: '70%',
+            innerRadius: '0%'
+        },
+
+        // label: {
+        //     text: 'Websales',
+        //     zindex: '10'
+        // }
+    }],
+
+
+
+// WORKING BOTTOM BUTTON 
+
+    // dockedItems: [{
+    //     xtype: 'toolbar',
+    //     dock: 'bottom',
+    //     // ui: 'footer',
+    //     layout: {
+    //         // align: 'middle',
+    //         pack: 'center',
+    //         // type: 'hbox'
+    //     },
+
+
+    //     defaults: {
+    //         // minWidth: 200
+    //     },
+    //     items: [
+    //         // { xtype: 'component', flex: 1 },
+    //         // { xtype: 'button', text: 'Button 1' },
+    //         {text: 'Today Web Sales v All Time High'}
+    //     ]
+    // }]
+
+
+    // BOTTOM BAR
+    // dockedItems: [{
+    //     xtype: 'toolbar',
+    //     dock: 'bottom',
+    //     items: [
+    //         { xtype: 'button', text: 'Button 1' }
+    //     ]
+    // }]
+
 
 });
+
+
+
+// OLD
+    // items: [{
+    //     reference: 'webSalesGauge',
+    //     xtype: 'gauge',
+    //     colors: ['#1F6D91', '#90BCC9'],
+    //     flex: 1,
+    //     radiusFactor: 10,
+    //     bind: '{value}',
+    //     needle: 'spike',
+    //     // label: {
+    //     //     text: 'Websales',
+    //     //     zindex: '10'
+    //     // }
+    // }],
+
+
+
+
+    // 
+
---
+++ classic/src/view/main/CustomerView.js
@@ -0,0 +1,17 @@
+// THIS IS AN UNUSED ITEM TEMPLATE
+// Ext.define('MyApp.view.main.CustomerView', {
+//     extend: 'Ext.view.View',
+//     xtype: 'dataview',
+//     alias: 'widget.customerview',
+//     scrollable: true,
+//     layout: {
+//         type: 'box',
+//         pack: 'space-around',
+//         wrap: true
+//     },
+//     itemTpl: [
+//         '<figure>',
+//         '<figcaption><div class=\'title\'>{title}</div><div class=\'firstName\'>{firstName}</div><div class=\'lastName\'>{lastName}</div></figcaption>',
+//         '</figure>'
+//     ]
+// });
